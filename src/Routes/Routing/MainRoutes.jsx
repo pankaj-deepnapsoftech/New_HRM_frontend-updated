092e79608a5ef96914ad9a4750a271fe3f156f8a
vishalpanchal1024 <vishalpanchal1024@gmail.com>
2025-05-19 15:56:37 +0530

    some change

--- src/Routes/Routing/MainRoutes.jsx
+++ src/Routes/Routing/MainRoutes.jsx
@@ -1,8 +1,14 @@
+import EmployeeTable from "@/pages/Employees";
 import MainDashboardPage from "@/pages/MainDashboardPage";
 
 export const MainRoutes = [
     {
         path:"/dashboard",
         element:<MainDashboardPage/>
+    },
+    {
+        path:"/employees",
+        element:<EmployeeTable/>
     }
+    
 ]
