9c86bff51a819f0fc702fc43c1facbb1b23428ac
nitinthakurdev <radheradhe9870023@gmail.com>
2025-09-30 12:25:07 +0530

    new changes

--- src/pages/UserPanel/UserDocument.jsx
+++ src/pages/UserPanel/UserDocument.jsx
@@ -1,6 +1,12 @@
-import React from 'react'
+import { useGetEmployeeDocumentDetailsQuery } from '@/service/Employee.services'
+import React, { useEffect } from 'react'
 
 const UserDocument = () => {
+  const { data: GetEmpDocument } = useGetEmployeeDocumentDetailsQuery()
+
+  useEffect(() => {
+
+  }, [])
   return (
     <div className='min-h-screen bg-gray-100 p-10'>
       <div className="bg-gray-400 rounded-xl shadow-md text-white py-4 px-6 text-center mb-12">
